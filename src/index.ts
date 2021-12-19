d61c149cadaa03dfaa74fc30a521cea49fc48d23
walrus811 <walrusray811@gmail.com>
2021-12-19 23:53:24 +0900

    불필요한 설정 제거

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
-
+import bodyParser from 'body-parser';
+app.use(express.json());
+app.use(express.urlencoded());
+console.log("hi");
 app.get('/', (_, res) =>
 {
   res.status(200).send("마이크 테스트");
 });
+app.post('/', (req, res) =>
+{
+  res.status(200).send("마이크 테스트");
+});
+
 app.listen(port, () => console.log(`listening on ${port}`));
