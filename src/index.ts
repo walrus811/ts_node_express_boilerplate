09aa5409d2cf672a8efe9157a745b55fdd61ab17
RayRay <walrusray811@gmail.com>
2021-12-20 10:54:30 +0900

    에러 핸들러 추가

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import express from 'express';
+import express, { ErrorRequestHandler } from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import httpErrors, { HttpError } from 'http-errors';
+import debug from 'debug';
+
+const initDebug = debug("init");
+
 dotenv.config();
+
 const app = express();
-const port = process.env.PORT;
-import bodyParser from 'body-parser';
+const port = parseInt(process.env.PORT as string) || 3000;
+
+
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded());
-console.log("hi");
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (_, res) =>
 {
   res.status(200).send("마이크 테스트");
@@ -16,4 +25,22 @@ app.post('/', (req, res) =>
   res.status(200).send("마이크 테스트");
 });
 
-app.listen(port, () => console.log(`listening on ${port}`));
+//404
+app.use(function RouteNotFound(req, res, next)
+{
+  next(httpErrors(404, `${req.originalUrl} Can't be Found`));
+});
+
+//500
+app.use(function HandleErrors(err: HttpError, req, res, next) 
+{
+  const resBody = {
+  } as { message?: string; };
+
+  if (err.expose)
+    resBody.message = err.message;
+
+  res.status(err.status || 500);
+  res.send(resBody);
+} as ErrorRequestHandler);
+app.listen(port, () => initDebug(`listening on ${port}`));
